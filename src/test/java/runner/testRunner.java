9a8dd1d1cfc411cb0b07d793533bfaf8bc6cb67f
Callum <65315388+24040461@users.noreply.github.com>
2023-03-20 11:23:08 +0000

    Update testRunner.java

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",
         glue= {"steps", "hooks"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",        "rerun:target/cucumber-reports/rerun.txt"})
 public class testRunner {
 
 }
